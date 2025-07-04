fde7792aa41487368a82b5ba63e077e048668940
Lkasta <lkastabackup@gmail.com>
2025-07-04 17:29:23 -0300

    feat(database): import db from database.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from database import db
+from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-@app.route("/hello-world", methods=['GET'])
+@app.route("/", methods=['GET'])
 def hello_jonas():
-  return "Jonas"
+    return "Jonas" 
 
 if __name__ == '__main__':
   app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -1,8 +1,7 @@
-from app import db
+from database import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Colunm(db.String(80), nullable=False, unique=True)
-  password = db.Column(db.string(80), nullable=False)
-
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)   # Column e String!
+    password = db.Column(db.String(80), nullable=False)
