96436eff1e8eff88755ee97e9b66bb34ba8a331b
Lkasta <lkastabackup@gmail.com>
2025-07-04 16:48:51 -0300

    feat(models): add User model

---
+++ models/user.py
@@ -0,0 +1,8 @@
+from app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Colunm(db.String(80), nullable=False, unique=True)
+  password = db.Column(db.string(80), nullable=False)
+
