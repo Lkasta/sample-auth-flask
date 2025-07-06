47fe128ff7c77d632eafd7d9baff392fe46e9f41
Lkasta <lkastabackup@gmail.com>
2025-07-05 23:34:53 -0300

    feat(routes): login route created

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from database import db
+from flask_login import LoginManager, login_user, current_user
+
 from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
+login_maneger = LoginManager()
 db.init_app(app)
+login_maneger.init_app(app)
+
+login_maneger.login_view = 'login'
+
+@app.route('/login', methods=['POST'])
+def login():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+    user = User.query.filter_by(username=username).first()
+    if user and user.password == password:
+      login_user(user)
+      print(current_user)
+      return jsonify({"message": "Login"}) 
+
+  return jsonify({"message": "Invalid credentials"}), 400
 
 @app.route("/", methods=['GET'])
 def hello_jonas():
-    return "Jonas" 
+  return "Jonas" 
 
 if __name__ == '__main__':
   app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -2,6 +2,6 @@ from database import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False, unique=True)   # Column e String!
-    password = db.Column(db.String(80), nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), nullable=False, unique=True)
+  password = db.Column(db.String(80), nullable=False)
