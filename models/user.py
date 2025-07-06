40407b35f790ae49d9f92c35f6a432d36c6b83af
Lkasta <lkastabackup@gmail.com>
2025-07-06 00:19:51 -0300

    feat(routes): login route created

--- models/user.py
+++ models/user.py
@@ -3,5 +3,5 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80), nullable=False, unique=True)
+  username = db.Column(db.String(80), nullable=False, unique=True)
   password = db.Column(db.String(80), nullable=False)
